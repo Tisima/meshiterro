f91db6002ff55b8227de53f6f2ea548b4d55a1d8
Tisima <zerosan487@gmail.com>
2022-08-01 04:26:08 +0000

    81

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -28,8 +28,8 @@ class PostImagesController < ApplicationController
   end
 
     private
-
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
+
